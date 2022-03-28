d7302b77a6c133e15c0db741e93f82ed16a0c4e4
Kang Hsieh <kanghsieh1990@gmail.com>
2022-03-28 11:13:11 +0100

    created routes for users with nested locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[show edit update] do
+    resources :locations, only: %i[index new create]
+  end
 end
