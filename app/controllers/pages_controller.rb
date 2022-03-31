3bec58c2a2cc48dc2e021cc4c85e5f1429d94e08
Kang Hsieh <kanghsieh1990@gmail.com>
2022-04-01 00:43:15 +0200

    added dashboard route and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    redirect_to dashboard_path if user_signed_in?
+  end
+
+  def dashboard
+    @user = current_user
   end
 end
