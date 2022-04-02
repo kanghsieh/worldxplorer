855e2a843c4b265dce5606b62ea9d7974d2dd28f
Kang Hsieh <kanghsieh1990@gmail.com>
2022-04-02 18:45:42 +0200

    setup geocoder gem and added address column to locations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_02_130744) do
+ActiveRecord::Schema.define(version: 2022_04_02_164434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2022_04_02_130744) do
     t.float "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
   create_table "users", force: :cascade do |t|
